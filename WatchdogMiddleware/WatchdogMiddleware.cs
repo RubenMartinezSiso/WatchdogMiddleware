59d28b81570534d3191e5a462e3af5e8356c8f8b
DESKTOP-9E97H6J\Ruben <ruben.martinez@techtiva.tech>
2025-01-14 13:08:36 +0100

    Error control, try-catch and logger

--- WatchdogMiddleware/WatchdogMiddleware.cs
+++ WatchdogMiddleware/WatchdogMiddleware.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using System.Text;
+using System;
+using System.IO;
+using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using InfluxDB.Client;
 using InfluxDB.Client.Api.Domain;
 using InfluxDB.Client.Writes;
-using InfluxDB.Client;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
 using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using WatchdogMiddleware.Models;
 
 namespace WatchdogMiddleware
@@ -22,97 +27,141 @@ namespace WatchdogMiddleware
         private readonly string _dataTable;
         private readonly bool _activateLogs;
 
-        private readonly HttpClient _httpClient = new HttpClient();
+        private readonly HttpClient _httpClient;
         private readonly RequestDelegate _next;
         private readonly ILogger<WatchdogMiddleware> _logger;
 
         public WatchdogMiddleware(RequestDelegate next, ILogger<WatchdogMiddleware> logger, string apiName, string influxDbUrl, string influxDbToken, string influxDbOrg, string influxDbBucket, string dataTable, bool activateLogs)
         {
-            _next = next;
-            _logger = logger;
-            _apiName = apiName;
-            _influxDbUrl = influxDbUrl;
-            _influxDbToken = influxDbToken;
-            _influxDbOrg = influxDbOrg;
-            _influxDbBucket = influxDbBucket;
-            _dataTable = dataTable;
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _apiName = apiName ?? "Unknown API";
+            _influxDbUrl = influxDbUrl ?? throw new ArgumentNullException(nameof(influxDbUrl));
+            _influxDbToken = influxDbToken ?? throw new ArgumentNullException(nameof(influxDbToken));
+            _influxDbOrg = influxDbOrg ?? throw new ArgumentNullException(nameof(influxDbOrg));
+            _influxDbBucket = influxDbBucket ?? throw new ArgumentNullException(nameof(influxDbBucket));
+            _dataTable = dataTable ?? throw new ArgumentNullException(nameof(dataTable));
             _activateLogs = activateLogs;
+            _httpClient = new HttpClient();
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
-            var startTime = DateTime.UtcNow;
-            var requestTask = Task.Run(() => LogRequest(context));
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
 
-            var originalBodyStream = context.Response.Body;
-            using var responseBody = new MemoryStream();
-            context.Response.Body = responseBody;
+            if (_activateLogs)
+            {
+                _logger.LogInformation("WatchdogMiddleware: Starting request processing");
+            }
+
+            var startTime = DateTime.UtcNow;
+            InterceptedRequest request = null;
+            InterceptedResponse response = null;
 
             try
             {
-                await _next(context);
+                request = await LogRequest(context);
+                var originalBodyStream = context.Response.Body;
+                using var responseBody = new MemoryStream();
+                context.Response.Body = responseBody;
 
-                responseBody.Seek(0, SeekOrigin.Begin);
-                await responseBody.CopyToAsync(originalBodyStream);
+                try
+                {
+                    await _next(context);
+                }
+                finally
+                {
+                    responseBody.Seek(0, SeekOrigin.Begin);
+                    await responseBody.CopyToAsync(originalBodyStream);
+                    response = await LogResponse(context.Response, startTime);
+                    context.Response.Body = originalBodyStream;
+                }
 
-                var responseTask = Task.Run(() => LogResponse(context.Response, startTime));
-
-                var request = await requestTask;
-                var response = await responseTask;
-
-                _ = Task.Run(() => WritePointToInfluxDB(request, response));
+                WritePointToInfluxDB(request, response);
             }
-            finally
+            catch (Exception ex)
             {
-                context.Response.Body = originalBodyStream;
+                if (_activateLogs)
+                {
+                    _logger.LogError(ex, "WatchdogMiddleware: An error occurred during request processing");
+                }
+            }
+
+            if (_activateLogs)
+            {
+                _logger.LogInformation("WatchdogMiddleware: Finished request processing");
             }
         }
 
         private async Task<InterceptedRequest> LogRequest(HttpContext context)
         {
-            var clientIp = context.Request.HttpContext.Connection.RemoteIpAddress?.ToString();
-            var (location, latitude, longitude) = await GetLocationFromIp(clientIp);
-
-            RouteData routeData = context.GetRouteData();
-            string action = routeData?.Values["action"]?.ToString() ?? "Unknown";
-            string controller = routeData?.Values["controller"]?.ToString() ?? "Unknown";
-
-            return new InterceptedRequest
+            try
             {
-                Timestamp = DateTime.UtcNow,
-                ApiName = _apiName != "" ? _apiName : "Unknown API",
-                Method = context.Request.Method,
-                Path = context.Request.Path,
-                QueryString = context.Request.QueryString.ToString(),
-                Headers = context.Request.Headers.ToDictionary(h => h.Key, h => h.Value.ToString()),
-                ClientIp = clientIp,
-                Host = Dns.GetHostName(),
-                Body = await GetRequestBodyAsync(context.Request),
-                Location = location,
-                Latitude = latitude,
-                Longitude = longitude,
-                RouteAction = action,
-                RouteController = controller
-            };
+                var clientIp = context.Request.HttpContext.Connection.RemoteIpAddress?.ToString();
+                var (location, latitude, longitude) = await GetLocationFromIp(clientIp);
+
+                RouteData routeData = context.GetRouteData();
+                string action = routeData?.Values["action"]?.ToString() ?? "Unknown";
+                string controller = routeData?.Values["controller"]?.ToString() ?? "Unknown";
+
+                return new InterceptedRequest
+                {
+                    Timestamp = DateTime.UtcNow,
+                    ApiName = _apiName,
+                    Method = context.Request.Method,
+                    Path = context.Request.Path,
+                    QueryString = context.Request.QueryString.ToString(),
+                    Headers = context.Request.Headers.ToDictionary(h => h.Key, h => h.Value.ToString()),
+                    ClientIp = clientIp,
+                    Host = Dns.GetHostName(),
+                    Body = await GetRequestBodyAsync(context.Request),
+                    Location = location,
+                    Latitude = latitude,
+                    Longitude = longitude,
+                    RouteAction = action,
+                    RouteController = controller
+                };
+            }
+            catch (Exception ex)
+            {
+                if (_activateLogs)
+                {
+                    _logger.LogError(ex, "WatchdogMiddleware: Error logging request");
+                }
+                return new InterceptedRequest();
+            }
         }
-        
-        private Task<InterceptedResponse> LogResponse(HttpResponse response, DateTime startTime)
+
+        private async Task<InterceptedResponse> LogResponse(HttpResponse response, DateTime startTime)
         {
-            return Task.Run(() => new InterceptedResponse
+            try
             {
-                Timestamp = DateTime.UtcNow,
-                Duration = DateTime.UtcNow - startTime,
-                StatusCode = response.StatusCode,
-                Headers = response.Headers.ToDictionary(h => h.Key, h => h.Value.ToString()),
-                Body = GetResponseBodyAsync(response.Body as MemoryStream).Result
-            });
+                return new InterceptedResponse
+                {
+                    Timestamp = DateTime.UtcNow,
+                    Duration = DateTime.UtcNow - startTime,
+                    StatusCode = response.StatusCode,
+                    Headers = response.Headers.ToDictionary(h => h.Key, h => h.Value.ToString()),
+                    Body = await GetResponseBodyAsync(response.Body as MemoryStream)
+                };
+            }
+            catch (Exception ex)
+            {
+                if (_activateLogs)
+                {
+                    _logger.LogError(ex, "WatchdogMiddleware: Error logging response");
+                }
+                return new InterceptedResponse();
+            }
         }
 
         private async Task<(string Location, double? Latitude, double? Longitude)> GetLocationFromIp(string ip)
         {
             try
             {
-                // ip = "80.26.158.41"; // Madrid, Spain
                 ip = "88.4.135.170"; // Fraga, Spain
                 var response = await _httpClient.GetStringAsync($"http://ip-api.com/json/{ip}");
                 var json = JObject.Parse(response);
@@ -125,53 +174,82 @@ namespace WatchdogMiddleware
                     json["lon"]?.Value<double>()
                 );
             }
-            catch
+            catch (Exception ex)
             {
+                if (_activateLogs)
+                {
+                    _logger.LogError(ex, "WatchdogMiddleware: Error getting location from IP");
+                }
                 return ("Unknown Location", null, null);
             }
         }
 
         private async Task<string> GetRequestBodyAsync(HttpRequest request)
         {
-            request.EnableBuffering();
-            using (var reader = new StreamReader(request.Body, Encoding.UTF8, true, 1024, true))
+            try
             {
-                var body = await reader.ReadToEndAsync();
-                request.Body.Seek(0, SeekOrigin.Begin);
-                return body;
+                request.EnableBuffering();
+                using (var reader = new StreamReader(request.Body, Encoding.UTF8, true, 1024, true))
+                {
+                    var body = await reader.ReadToEndAsync();
+                    request.Body.Seek(0, SeekOrigin.Begin);
+                    return body;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (_activateLogs)
+                {
+                    _logger.LogError(ex, "WatchdogMiddleware: Error reading request body");
+                }
+                return string.Empty;
             }
         }
 
         private async Task<string> GetResponseBodyAsync(MemoryStream responseBody)
         {
-            if (responseBody == null) return string.Empty;
+            try
+            {
+                if (responseBody == null) return string.Empty;
 
-            responseBody.Seek(0, SeekOrigin.Begin);
-            var body = await new StreamReader(responseBody).ReadToEndAsync();
-            responseBody.Seek(0, SeekOrigin.Begin);
-            return body;
+                responseBody.Seek(0, SeekOrigin.Begin);
+                var body = await new StreamReader(responseBody).ReadToEndAsync();
+                responseBody.Seek(0, SeekOrigin.Begin);
+                return body;
+            }
+            catch (Exception ex)
+            {
+                if (_activateLogs)
+                {
+                    _logger.LogError(ex, "WatchdogMiddleware: Error reading response body");
+                }
+                return string.Empty;
+            }
         }
 
-        public async Task WritePointToInfluxDB(InterceptedRequest request, InterceptedResponse response)
+        private void WritePointToInfluxDB(InterceptedRequest request, InterceptedResponse response)
         {
+            if (request == null || response == null)
+            {
+                if (_activateLogs)
+                {
+                    _logger.LogError("WatchdogMiddleware: Cannot write to InfluxDB, request or response is null");
+                }
+                return;
+            }
+
             try
             {
                 using var client = InfluxDBClientFactory.Create(_influxDbUrl, _influxDbToken.ToCharArray());
                 var writeApi = client.GetWriteApi();
 
                 var point = PointData.Measurement(_dataTable)
-
-                    // Timestamp
                     .Timestamp(request.Timestamp, WritePrecision.Ms)
-
-                    // Tags
                     .Tag("req_api", request.ApiName)
                     .Tag("req_method", request.Method)
                     .Tag("req_path", request.Path)
                     .Tag("req_route_action", request.RouteAction)
                     .Tag("req_route_controller", request.RouteController)
-
-                    // Request Fields
                     .Field("req_host", request.Host)
                     .Field("req_timestamp", request.Timestamp.ToString("o"))
                     .Field("req_query_string", request.QueryString)
@@ -181,19 +259,20 @@ namespace WatchdogMiddleware
                     .Field("req_longitude", request.Longitude)
                     .Field("req_body", request.Body)
                     .Field("req_headers", JsonConvert.SerializeObject(request.Headers))
-
-                    // Response Fields
                     .Field("res_timestamp", response.Timestamp.ToString("o"))
                     .Field("res_status_code", response.StatusCode)
                     .Field("res_body", response.Body)
                     .Field("res_duration_ms", response.Duration.TotalMilliseconds)
                     .Field("res_headers", JsonConvert.SerializeObject(response.Headers));
 
-                await Task.Run(() => writeApi.WritePoint(point, _influxDbBucket, _influxDbOrg));
+                writeApi.WritePoint(point, _influxDbBucket, _influxDbOrg);
             }
             catch (Exception ex)
             {
-                _logger.LogError($"Error writing to InfluxDB: {ex.Message}");
+                if (_activateLogs)
+                {
+                    _logger.LogError(ex, "WatchdogMiddleware: Error writing to InfluxDB");
+                }
             }
         }
     }
